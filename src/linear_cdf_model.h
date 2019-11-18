b9a81b8369e08953e2a2cf28e7dac98649f4ec71
Adam Jaffe <azjaffe1@gmail.com>
2019-11-18 00:06:12 -0500

    Restructure cdf model builder
      * There is a bug in the linear model (I think)... debug this tomorrow

--- src/linear_cdf_model.h
+++ src/linear_cdf_model.h
@@ -7,31 +7,6 @@ using namespace std;
 #include <vector>
 #include <iostream>
 
-// D is the dimension of the data
-template <uint D>
-class PTree {
-    double data[]; // For now, index one array instead of leaves pointing to data pages
-    uint fanout;
-    uint height; // e.g. a tree where the root is leaf has height 1
-
-    struct node {
-        node * [MAX_FANOUT] children;
-        double[D] projection;
-        linearModel model;
-        isLeaf bool;
-        // start and end will only be used if isLeaf is true.
-        // they correspond to the starting and ending indecies of the data this leaf indexes
-        int start; 
-        int end;
-    };
-    public:
-    node * root;
-    PTree(double data[], uint fanout, uint height) :
-    fanout(fanout), height(height) {
-        
-    }
-};
-
 struct linearModel {
 public:
     double bias;
@@ -45,20 +20,54 @@ public:
     }
 };
 
-// Assuming data is made up of unique values
-// TODO? remove this assumption
-linearModel fitLinearCdf(vector<double> data) {
-    double n = data.size();
-    double s_xy = 0, s_x = 0, s_xx = 0;
-    double s_y = n * (n+1) / 2;
-    for(int i = 0; i < n; i++) {
-        s_xy += data[i] * (i+1);
-        s_x += data[i];
-        s_xx += data[i] * data[i];
+// D is the dimension of the data
+template <uint D>
+class PTree {
+    double * data; // For now, index one array instead of leaves pointing to data pages
+    uint fanout;
+    uint height; // e.g. a tree where the root is leaf has height 1
+
+    struct node {
+        node children[MAX_FANOUT];
+        double projection[D];
+        linearModel model;
+        bool isLeaf;
+        // start and end will only be used if isLeaf is true.
+        // they correspond to the starting and ending indecies of the data this leaf indexes
+        int start; 
+        int end;
+    };
+    public:
+    node * root;
+    PTree(double data[], uint fanout, uint height) :
+    fanout(fanout), height(height), data(data) {
+
     }
-    double slope = (s_xy - s_x*s_y/n) / (n*s_xx - s_x*s_x);
-    return linearModel(
-        (s_y/n - slope*s_x) / n,
-        slope
-    );
-}
+};
+
+// linearCdfModelBuilder builds a linear model from
+// a stream of data points in sorted order.
+// (1) Pass the (sorted) data points one at a time into add
+// (2) Call build()
+// TODO(?): This class assumes data is made up of unique values, remove this assumption if necessary
+class linearCdfModelBuilder {
+public:
+    double s_xy, s_x, s_xx;
+    uint n; // number of data points seen so far
+    linearCdfModelBuilder() : s_xy(0), s_x(0), s_xx(0), n(0) {}
+    void add(double d) {
+        n++;
+        s_xy += d * n;
+        s_x += d;
+        s_xx += d * d;
+    }
+    linearModel build() {
+        double s_y = (n+1) / 2;
+        cout << "s_y=" << s_y << " s_xy=" << s_xy << " s_x=" << s_x << " s_xx=" << s_xx << endl;
+        double slope = (s_xy - s_x*s_y) / (n*s_xx - s_x*s_x);
+        return linearModel(
+            (s_y - slope*s_x) / n,
+            slope
+        );
+    }
+};
