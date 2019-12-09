4dcb809331a2623559124188ce12b6c2e8ea0968
x74feng <xiyangf1997@gmail.com>
2019-12-09 16:50:10 -0500

    add data generator

---
+++ data/config.py
@@ -0,0 +1,19 @@
+from enum import Enum
+
+# default dimension
+DIM = 2
+# default number of points
+DATA_SIZE = 1000000
+# default number of queries
+QUERY_SIZE = 1000
+
+# supported data distributions
+class Distribution(Enum):
+
+    RANDOM = 0
+    NORMAL = 1
+    
+parseDistribution = {
+    Distribution.RANDOM : "random",
+    Distribution.NORMAL : "normal",
+}
