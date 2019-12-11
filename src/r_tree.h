d6e253091031da75de5331e624930d224523b683
Adam Jaffe <azjaffe1@gmail.com>
2019-12-11 17:21:09 -0500

    Add r tree implementation

---
+++ src/r_tree.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include <iostream>
+#include <random>
+#include <chrono>
+#include <spatialindex/SpatialIndex.h>
+#include <spatialindex/capi/CountVisitor.h>
+
+// Wrapper around spatialindex Rtree
+template <uint D, typename V>
+// Doesnt use V
+class RTree {
+    typedef pair<array<double, D>, V> datum;
+    SpatialIndex::ISpatialIndex * t;
+public:
+    RTree(vector<datum> & data) {
+        SpatialIndex::IStorageManager * memStore = SpatialIndex::StorageManager::createNewMemoryStorageManager();
+        SpatialIndex::id_type id = 0;
+        t = SpatialIndex::RTree::createNewRTree(
+            *memStore,
+            0.5, // fill factor
+            1000, // index capacity
+            1000, // leaf capacity,
+            D, // dimension
+            SpatialIndex::RTree::RV_RSTAR, // type
+            id // id (not sure why this is needed)
+        ); // TODO - tune parameters?
+        for(auto it = data.begin(); it != data.end(); it++) {
+            cout << "Adding data point: " << it - data.begin() << endl;
+            SpatialIndex::id_type id = 0;
+            t->insertData(
+                0, nullptr, // Don't insert data, just the point
+                SpatialIndex::Point(it->first.data(), D),
+                id
+            );
+        }
+    };
+
+    size_t rangeQuery(array<double, D> min, array<double, D> max) {
+        auto r = SpatialIndex::Region(min.data(), max.data(), D);
+        auto v = CountVisitor();
+        t->intersectsWithQuery(r, v);
+        return v.GetResultCount();
+    }
+};
